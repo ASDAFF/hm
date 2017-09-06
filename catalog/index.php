34aa7dfc3c12b3b815acee275ec494f8992b8353
Tim <tim.kukom@gmail.com>
2017-09-06 12:47:02 +0400

    Наценка

--- catalog/index.php
+++ catalog/index.php
@@ -36,7 +36,7 @@ $APPLICATION->SetTitle("Каталог товаров");
 			1 => "",
 		),
 		"FILTER_PRICE_CODE" => array(
-			0 => "BASE",
+			0 => "ESTORE",
 		),
 		"FILTER_OFFERS_FIELD_CODE" => array(
 			0 => "",
@@ -143,7 +143,7 @@ $APPLICATION->SetTitle("Каталог товаров");
 		"COMPARE_ELEMENT_SORT_ORDER" => "asc",
 		"DISPLAY_ELEMENT_SELECT_BOX" => "N",
 		"PRICE_CODE" => array(
-			0 => "BASE",
+			0 => "ESTORE",
 		),
 		"USE_PRICE_COUNT" => "N",
 		"SHOW_PRICE_COUNT" => "1",
